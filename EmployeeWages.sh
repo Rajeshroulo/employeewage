43af8cbff5003c0aeac9910715c87fc895ed4115
Rajeshroulo <rajraval017@gmail.com>
2020-05-03 12:37:22 +0530

    Refactored the whole code for all UseCases

---
+++ EmployeeWages.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -x
+
+isParttime=1
+isFulltime=2
+RatePerHr=20
+workingdays=20
+Hrsinmonth=4
+
+totalHrs=0
+totalworkingdays=0
+
+function getworkingHrs( ){
+  case $1 in
+        $isParttime )
+          Hrs=4
+          ;;
+        $isFulltime )
+          Hrs=8
+          ;;
+        * )
+          Hrs=0
+          ;;
+  esac
+ echo $Hrs
+}
+
+function Dailywage( ) {
+  local Hrs=$1
+  wage=$(($Hrs*$RatePerHr))
+  echo $wage
+}
+
+while [[ $totalHrs -lt $Hrsinmonth && $totalworkingdays -lt $workingdays ]]
+do
+  ((totalworkingdays++))
+   workHrs="$( getworkingHrs $((RANDOM%3)) )"
+   totalHrs=$(($totalHrs+$workHrs))
+   empdailywage[$totalworkingdays]="$( Dailywage $Hrs )"
+done
+
+totalsalary="$( Dailywage $totalHrs )"
+echo "Daily Wage" ${empdailywage[@]}
+echo "All keys " ${!empdailywage[@]}
